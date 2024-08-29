c4cd8450a343b7fee2a5c42a489e964efa86f9c1
Varshini10-02 <151113488+Varshini10-02@users.noreply.github.com>
2024-08-29 17:49:13 +0530

    Create app.js

---
+++ Experiment-8-9/server/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser=require('body-parser')
+const mongoose = require('mongoose')
+const cors=require('cors')
+const url = 'mongodb://localhost/CBIT'
+const app = express()
+const studentsrouter = require('./routes/student')
+mongoose.connect(url, {useNewUrlParser:true})
+const con = mongoose.connection
+con.on('open', () =>
+{
+console.log('connected...')
+})
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json())
+app.use('/students',studentsrouter)
+app.listen(5000, () =>
+{
+console.log('Server started')
+})
